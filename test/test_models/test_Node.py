c9077dca01a8b2279764350c259fa66ba10bb44e
Johannes Ekberg <uppfinnarn@gmail.com>
2014-11-10 15:59:20 +0100

    More unit tests, I just found a bug

--- test/test_models/test_Node.py
+++ test/test_models/test_Node.py
@@ -11,11 +11,25 @@ class TestNode(unittest.TestCase):
 		self.assertEqual(self.node.host, '10.2.0.30')
 		self.assertIsNone(self.node.username)
 	
-	def test_load_data_host_and_username(self):
+	def test_load_data_username(self):
 		self.node.load_data("admin@10.2.0.30")
 		self.assertEqual(self.node.scheme, 'http')
 		self.assertEqual(self.node.host, '10.2.0.30')
 		self.assertEqual(self.node.username, 'admin')
+	
+	def test_load_data_username_password(self):
+		self.node.load_data("admin:password@10.2.0.30")
+		self.assertEqual(self.node.scheme, 'http')
+		self.assertEqual(self.node.host, '10.2.0.30')
+		self.assertEqual(self.node.username, 'admin')
+		self.assertEqual(self.node.password, 'password')
+	
+	def test_load_data_username_password_protocol(self):
+		self.node.load_data("http://admin:password@10.2.0.30")
+		self.assertEqual(self.node.scheme, 'http')
+		self.assertEqual(self.node.host, '10.2.0.30')
+		self.assertEqual(self.node.username, 'admin')
+		self.assertEqual(self.node.password, 'password')
 	
 	def test_load_data_https(self):
 		self.node.load_data("https://admin@10.2.0.30")

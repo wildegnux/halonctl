7722c50f1f6731bcb516acd62fcb95f534c86e85
Johannes Ekberg <johannes@halon.se>
2014-11-12 18:34:00 +0100

    Function to ask for confirmation

--- halon/util.py
+++ halon/util.py
@@ -8,3 +8,22 @@ def make_table(rows):
 
 def print_table(*args, **kwargs):
 	print make_table(*args, **kwargs).draw()
+
+def ask_confirm(prompt, default=True):
+	if type(default) != bool:
+		raise TypeError("The default value for ask_confirm must be a bool!")
+	
+	answers = {
+		'y': True, 'yes': True,
+		'n': False, 'no': False,
+		'': default
+	}
+	suffixes = { True: '[Yn]', False: '[yN]' }
+	
+	while True:
+		answer = raw_input("%s %s:" % (prompt, suffixes[default])).lower()
+		if answer in answers:
+			return answers[answer]
+		else:
+			print "Enter either y/yes or n/no, or nothing for default (%s)" % \
+				('yes' if default else 'no')

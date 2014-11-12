565133134c3c9a5f7407b45b4d59cbb7e9dc65f2
Johannes Ekberg <johannes@halon.se>
2014-11-12 16:10:58 +0100

    Allow modules to have submodules
    This can go on for all eternity~

--- halon/modules.py
+++ halon/modules.py
@@ -2,8 +2,17 @@ class Module(object):
 	exitcode = 0
 	partial = False
 	
+	submodules = {}
+	
 	def register_arguments(self, parser):
-		pass
+		# The default implementation adds subcommands if there are any
+		if self.submodules:
+			subparsers = parser.add_subparsers(metavar='subcommand')
+			for name, mod in self.submodules.iteritems():
+				p = subparsers.add_parser(name, help=mod.__doc__)
+				p.set_defaults(_mod=mod)
 	
 	def run(self, nodes, args):
-		pass
+		# The default implementation simply delegates to a subcommand
+		if self.submodules:
+			return args._mod.run(nodes, args)

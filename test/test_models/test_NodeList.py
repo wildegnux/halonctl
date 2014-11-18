c06cc6a842fd0a45e1584a477a9f8799dd7dabe0
Johannes Ekberg <johannes@halon.se>
2014-11-18 13:42:10 +0100

    Refactored sync_credentials() tests

--- test/test_models/test_NodeList.py
+++ test/test_models/test_NodeList.py
@@ -3,12 +3,11 @@ from halonctl.models import Node, NodeList
 
 class TestNodeList(unittest.TestCase):
 	def setUp(self):
-		self.cluster = NodeList([
-			Node("http://0.0.0.1", 'n1'),
-			Node("http://0.0.0.2", 'n2'),
-			Node("http://0.0.0.3", 'n3'),
-			Node("http://0.0.0.4", 'n4')
-		])
+		self.cluster = NodeList()
+		self.cluster.append(Node("http://0.0.0.1", 'n1', self.cluster))
+		self.cluster.append(Node("http://0.0.0.2", 'n2', self.cluster))
+		self.cluster.append(Node("http://0.0.0.3", 'n3', self.cluster))
+		self.cluster.append(Node("http://0.0.0.4", 'n4', self.cluster))
 	
 	def test_load_data_with_username(self):
 		self.cluster.load_data({'username': 'admin'})
@@ -22,27 +21,24 @@ class TestNodeList(unittest.TestCase):
 		self.cluster.load_data([])
 		self.assertIsNone(self.cluster.username)
 	
-	def test_sync_credentials(self):
+	def test_shared_credentials(self):
 		self.cluster.load_data({'username': 'admin', 'password': 'password'})
-		self.cluster.sync_credentials()
 		for node in self.cluster:
 			self.assertEqual(node.username, 'admin')
 			self.assertEqual(node.password, 'password')
 	
-	def test_sync_credentials_node_username(self):
+	def test_shared_credentials_node_username(self):
 		self.cluster[1].username = 'admin'
 		self.cluster[1].password = 'password'
-		self.cluster.sync_credentials()
 		for node in self.cluster:
 			self.assertEqual(node.username, 'admin')
 			self.assertEqual(node.password, 'password')
 	
-	def test_sync_credentials_nodes_can_override(self):
+	def test_shared_credentials_nodes_can_override(self):
 		self.cluster[1].username = 'admin'
 		self.cluster[1].password = 'password'
 		self.cluster[2].username = 'admib'
 		self.cluster[2].password = 'passbord'
-		self.cluster.sync_credentials()
 		self.assertEqual(self.cluster[0].username, 'admin')
 		self.assertEqual(self.cluster[0].password, 'password')
 		self.assertEqual(self.cluster[1].username, 'admin')

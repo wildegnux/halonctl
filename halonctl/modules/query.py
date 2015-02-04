43cb25d68883afbe06d8a4929ca7f6f06fbd005d
Erik Lax <erik@halon.se>
2015-02-04 14:59:31 +0100

    Print selected fields with --fields

--- halonctl/modules/query.py
+++ halonctl/modules/query.py
@@ -14,6 +14,8 @@ class QueryModule(Module):
 			help="limit when just showing emails (default: 100)")
 		parser.add_argument('--debug-hql', action='store_true',
 			help="print resulting hql queries, for debugging")
+		parser.add_argument('--fields',
+			help="print selected fields")
 		
 		tzgroup = parser.add_mutually_exclusive_group()
 		tzgroup.add_argument('--utc', dest='timezone', action='store_const', const=0,
@@ -45,7 +47,42 @@ class QueryModule(Module):
 			print "--history cannot be used together with actions!"
 			self.exitcode = 1
 			return
-		
+
+		if args.action and args.fields:
+			print "--fields cannot be used together with actions!"
+			self.exitcode = 1
+			return
+
+		# Build an array with all supported fields for a particular source
+		supported_fields = ['action', 'actionid', 'cluster', 'from', 'ip',
+			'messageid', 'node', 'queueid', 'sasl', 'server', 'size', 'subject',
+			'time', 'to', 'transport']
+		if args.history:
+			supported_fields.append('historyid')
+		else:
+			supported_fields.append('quarantine')
+			supported_fields.append('retry')
+
+		if args.fields:
+			if args.fields == '-':
+				fields = supported_fields
+			else:
+				fields = args.fields.split(',')
+		else:
+			if args.history:
+				fields = ['cluster', 'node', 'messageid', 'from', 'to', 'subject']
+			else:
+				fields = ['cluster', 'node', 'messageid', 'queueid', 'from', 'to', 'subject']
+
+		for f in fields:
+			if not f in supported_fields:
+				print "Field '{0}' is not available!".format(f)
+				print "Available fields:"
+				for p in supported_fields:
+					print " {0}".format(p)
+				self.exitcode = 1
+				return
+
 		# Timestamp placeholders need a timezone specified!
 		for s in args.filter:
 			if args.timezone is None and filter_timestamp_re.search(s):
@@ -60,7 +97,7 @@ class QueryModule(Module):
 		
 		# Dispatch!
 		if args.action is None:
-			return self.do_show(nodes, args, hql)
+			return self.do_show(nodes, args, hql, fields)
 		elif args.action == "deliver":
 			return self.do_deliver(nodes, args, hql, False)
 		elif args.action == "deliver-duplicate":
@@ -68,8 +105,8 @@ class QueryModule(Module):
 		elif args.action == "delete":
 			return self.do_delete(nodes, args, hql)
 	
-	def do_show(self, nodes, args, hql):
-		yield ('Cluster', 'Node', 'From', 'To', 'Subject')
+	def do_show(self, nodes, args, hql, fields):
+		yield fields
 		
 		source = getattr(nodes.service, 'mailHistory' if args.history else 'mailQueue')
 		for node, result in source(filter=hql, offset=args.offset or None, limit=args.limit or 100).iteritems():
@@ -78,8 +115,27 @@ class QueryModule(Module):
 				self.partial = True
 			elif 'item' in result[1]['result']:
 				for msg in result[1]['result']['item']:
-					msg['msgsubject'] = from_base64(msg['msgsubject'])
-					yield (node.cluster.name, node.name, msg['msgfrom'], msg['msgto'], msg['msgsubject'])
+					p = []
+					for f in fields:
+						if f == 'action': p.append(msg['msgaction'])
+						elif f == 'actionid': p.append(msg['msgactionid'])
+						elif f == 'cluster': p.append(node.cluster.name)
+						elif f == 'from': p.append(msg['msgfrom'])
+						elif f == 'historyid': p.append(msg['id'])
+						elif f == 'ip': p.append(msg['msgfromserver'])
+						elif f == 'messageid': p.append(msg['msgid'])
+						elif f == 'node': p.append(node.name)
+						elif f == 'quarantine': p.append(msg['msgquarantine'])
+						elif f == 'queueid': p.append(msg['msgqueueid' if args.history else 'id'])
+						elif f == 'retry': p.append(msg['msgretries'])
+						elif f == 'sasl': p.append(msg['msgsasl'])
+						elif f == 'server': p.append(msg['msglistener'])
+						elif f == 'size': p.append(msg['msgsize'])
+						elif f == 'subject': p.append(from_base64(msg['msgsubject']))
+						elif f == 'time': p.append(msg['msgts0'])
+						elif f == 'to': p.append(msg['msgto'])
+						elif f == 'transport': p.append(msg['msgtransport'])
+					yield p
 	
 	def do_deliver(self, nodes, args, hql, duplicate):
 		if not hql and not ask_confirm("You have no filter, do you really want to try to deliver everything?", False):

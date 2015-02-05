wildegnux/halonctl

commit 957e6033f117e6fa80e7f5abb60a12164be22a48

Nicer formatting of dates

halonctl/modules/query.py
@@ -1,6 +1,6 @@
 import argparse, datetime
 from halonctl.modapi import Module
-from halonctl.util import hql_from_filters, filter_timestamp_re, ask_confirm, from_base64
+from halonctl.util import hql_from_filters, filter_timestamp_re, ask_confirm, from_base64, get_date
 
 class QueryModule(Module):
 	'''Queries emails and performs actions'''
@@ -129,7 +129,7 @@ class QueryModule(Module):
 						elif f == 'server': p.append(msg['msglistener'])
 						elif f == 'size': p.append(msg['msgsize'])
 						elif f == 'subject': p.append(from_base64(msg['msgsubject']))
-						elif f == 'time': p.append(msg['msgts0'] if args.verbose else datetime.datetime.utcfromtimestamp(int(msg['msgts0']) + (args.timezone*60*60 if args.timezone else 0)))
+						elif f == 'time': p.append(msg['msgts0'] if args.verbose else get_date(msg['msgts0'], args.timezone).format())
 						elif f == 'to': p.append(msg['msgto'])
 						elif f == 'transport': p.append(msg['msgtransport'])
 					yield p

halonctl/util.py
@@ -61,6 +61,10 @@ def ask_confirm(prompt, default=True):
 			continue
 		return answers[answer]
 
+def get_date(s, timezone=0):
+	'''Returns a timezone-adjusted date as an arrow object'''
+	return arrow.get(arrow.get(s).naive, tz.tzoffset(None, timezone*60*60 if timezone else 0))
+
 filter_timestamp_re = re.compile(r'\{([^}]*)\}')
 def hql_from_filters(filters, timezone=0):
 	'''Gets a HQL statement from a list of filter components.
@@ -72,12 +76,9 @@ def hql_from_filters(filters, timezone=0):
 	:param list filters: A list of filters to glue together
 	:param int timezone: The UTC offset of the assumed timezone
 	'''
-	def get_date(s):
-		return arrow.get(arrow.get(s).naive, tz.tzoffset(None, timezone*60*60))
-	
 	conditions = []
 	for s in filters:
-		s = filter_timestamp_re.sub(lambda m: str(get_date(m.groups(0)[0]).timestamp), s)
+		s = filter_timestamp_re.sub(lambda m: str(get_date(m.groups(0)[0]).timestamp), lambda s: get_date(s, timezone))
 		conditions.append(s)
 	
 	return ' '.join(conditions)

commit 4c09ff356dda29a7a89853361a75768f1e9d96a6

Fixed regression in 957e6033f117e6fa80e7f5abb60a12164be22a48

halonctl/modules/query.py
@@ -129,7 +129,7 @@ class QueryModule(Module):
 						elif f == 'server': p.append(msg['msglistener'])
 						elif f == 'size': p.append(msg['msgsize'])
 						elif f == 'subject': p.append(from_base64(msg['msgsubject']))
-						elif f == 'time': p.append(msg['msgts0'] if args.verbose else get_date(msg['msgts0'], args.timezone).format())
+						elif f == 'time': p.append(msg['msgts0'] if args.verbose else get_date(msg['msgts0'], args.timezone))
 						elif f == 'to': p.append(msg['msgto'])
 						elif f == 'transport': p.append(msg['msgtransport'])
 					yield p
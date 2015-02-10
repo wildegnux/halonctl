aebcef0b4551f45c5f3c2a09e089266f39e7e43b
Johannes Ekberg <johannes@halon.se>
2015-02-10 14:58:39 +0100

    This is no longer intended as a template

--- halonctl/modules/status.py
+++ halonctl/modules/status.py
@@ -3,58 +3,16 @@ import six
 from halonctl.modapi import Module
 import datetime
 
-# This is intended as kind of a template module, and thus will be more
-# extensively documented than anything else. Reading this should give you a
-# fairly good idea of how to write your own modules.
-# 
-# First off, the command name is inferred from the filename, minus the .py
-# suffix. Thus, calling 'halonctl status' will call the module contained in
-# this file.
-# 
-# Second, see that triple-single-quoted string at the top of the StatusModule
-# class? That's called a Docstring - a string with no variable name, at the
-# very top of a block. It gets automatically assigned to the __doc__ variable,
-# where halonctl will find it, and use it as a description for the --help page.
-# 
-# Third, the 'module' variable is expected to contain a halon.modules.Module
-# instance, otherwise it will complain on startup about module being missing.
-# 
-
 class StatusModule(Module):
 	'''Checks node statuses'''
 	
 	def run(self, nodes, args):
-		# 
-		# This runs when the command is actually called, eg. `halonctl status`.
-		# 
-		# The 'nodes' argument is a NodeList containing the nodes you're
-		# supposed to target. NodeList is a normal list, with a few extra
-		# attributes, the only one of interest here being 'service', which is a
-		# proxy that allows you to target a group of Nodes with a SOAP call.
-		# 
-		# If you would target all nodes with the same call anyways, it's
-		# recommended that you use this proxy - in the future, it will gain the
-		# ability to target all nodes in parallel, greatly speeding things up.
-		# 
-		# There are two ways to print to the console from run(): Use normal
-		# print statements, or `yield` a tuple of table headers, then any
-		# number of rows - with the latter, it will automatically be rendered
-		# into an appropriately formatted table when the function returns.
-		# 
-		# Your custom arguments will be available on the args object, as with
-		# args.verbose below.
-		# 
-		
 		yield (u"Cluster", u"Name", u"Address", u"Uptime", u"Status")
 		
 		for node, result in six.iteritems(nodes.service.getUptime()):
-			# If some nodes cannot be reached, mark the results as partial
-			# This will cause halonctl to exit with status 99 at the end, unless
-			# the --ignore-partial flag is set.
 			if result[0] != 200:
 				self.partial = True
 			
-			# Use the excellent Arrow library to format the uptime
 			uptime = None
 			if result[0] == 200:
 				uptime = datetime.timedelta(seconds=result[1])

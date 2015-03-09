6ce9c39ced07907cf4b79199ccca9557f6d3bd39
Johannes Ekberg <johannes@halon.se>
2015-03-09 15:43:45 +0100

    Interactive shell module
    This is a Python interpreter, with 'nodes' and 'args' exposed, along with a few convenience imports; useful for trying things out, experimenting with SOAP calls, etc

---
+++ halonctl/modules/shell.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import sys
+import six
+from inspect import getmembers
+from code import InteractiveConsole
+from halonctl import __version__ as version
+from halonctl.modapi import Module
+
+def dict_import(modname, members=['*']):
+	mod = __import__(modname, globals(), locals(), members, 0)
+	return dict(getmembers(mod))
+
+class ShellModule(Module):
+	'''Starts an interactive Python interpreter'''
+	
+	def run(self, nodes, args):
+		# Banner
+		banner = u'\n'.join([
+			u"Python {pyversion[0]}.{pyversion[1]}.{pyversion[2]}, halonctl {version}",
+			u"Variables: nodes, args",
+			u"",
+			u"{ps1}from __future__ import print_function",
+			u"{ps1}import six",
+			u"{ps1}from halonctl.util import *",
+			u"",
+		]).format(pyversion=sys.version_info, version=version, ps1='>>> ')
+		
+		# Shell context locals
+		locals_ = {
+			# Mimic Python's interactive sessions
+			'__name__': '__console__',
+			'__doc__': None,
+			
+			# Provide some variables
+			'nodes': nodes,
+			'args': args
+		}
+		
+		# Mimic imports
+		locals_.update(dict_import('__future__', 'print_function'))
+		locals_.update(dict_import('halonctl.util'))
+		
+		# Set up readline for history navigation
+		try:
+			import readline
+			import rlcompleter
+		except ImportError:
+			# We're on Windows, what's a readline?
+			pass
+		else:
+			completion_vars = globals()
+			completion_vars.update(locals_)
+			readline.set_completer(rlcompleter.Completer(completion_vars).complete)
+			readline.parse_and_bind('tab: complete')
+		
+		# Run an interactive console session
+		console = InteractiveConsole(locals_)
+		console.interact(banner)
+
+module = ShellModule()
